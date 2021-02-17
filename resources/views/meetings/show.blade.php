ca867c34a7752c81e15f6ee77be07080890a4a71
Whitney Harden <wtharden320@gmail.com>
2021-02-17 04:25:41 -0500

    added form on meeting page

--- resources/views/meetings/show.blade.php
+++ resources/views/meetings/show.blade.php
@@ -16,6 +16,31 @@
     <div>{{$meeting->start->format('Y M d @ H:i')}} - {{$meeting->end->format('Y M d @ H:i')}}</div>
 
     <!-- here is where a form allowing users to toggle on/off the public meeting page - you will need to handle that form inside of a controller -->
+    <h2>RSVP for this Meeting</h2>
+    <div class="container">
+        <div class="row">
+
+    <form>
+        <div class="col-6">
+
+        <pre>email</pre>
+        <input type="email">
+        </div>
+        <div class="col-6">
+
+        <pre>name</pre>
+        <input type="text">
+        </div>
+        <pre>RSVP 'yes' or 'no'</pre>
+        <p>Will you be attending this meeting?</p>
+        <input type="radio" id="rsvpyes" name="rsvpyes" value="yes">
+        <label for="rsvpyes">Yes</label><br>
+        <input type="radio" id="rsvpno" name="rsvpno" value="no">
+        <label for="rsvpno">No</label><br>
+        <button>Submit</button>
+    </form>
+        </div>
+    </div>
 
     <h2>RSVPs</h2>
     <!-- here is where a list of emails with their RSVP status for this meeting will go -->
